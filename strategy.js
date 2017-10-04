78c8be47c745e127f6a8e63798e16b095173e286
Scott Serage <xttocsurge51@gmail.com>
2017-10-04 15:09:54 -0600

    completed steps 1 and 2

---
+++ strategy.js
@@ -0,0 +1,15 @@
+const Auth0Strategy = require('passport-auth0')
+    , config = require(`${__dirname}/config.js`);
+
+const {domain, clientID, clientSecret} = config;
+
+module.exports = new Auth0Strategy({
+    domain,
+    clientID,
+    clientSecret,
+    callbackURL: '/login'
+  },
+  function(accessToken, refreshToken, extraParams, profile, done){
+    return done(null, profile);
+  }
+);
